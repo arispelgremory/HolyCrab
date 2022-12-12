73042037a19863425b03807704a5d3d7e0634134
gremoryyx <arispelgremory@gmail.com>
2022-12-12 14:06:10 +0800

    set crab's animation link to movement.

--- Assets/Scripts/CrabMovement.cs
+++ Assets/Scripts/CrabMovement.cs
@@ -3,33 +3,61 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
-using Vector3 = UnityEngine.Vector3;
 
 public class CrabMovement : MonoBehaviour
 {
 
-    public float speed;
+    public float speed = 15;
 
     private Rigidbody rb;
+    private Animator anim;
+    
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        float v = Input.GetAxisRaw("Vertical");
-        float h = Input.GetAxisRaw("Horizontal");
+    {   
+        // Movement
+        if (rb.velocity.z > 0)
+        {
+            anim.SetBool("IsForward", true);
+            Debug.Log("Moving Forward");
+        } else if (rb.velocity.z < 0)
+        {
+            anim.SetBool("IsBackward", true);
+            Debug.Log("Moving Backwards");
+        } else if (rb.velocity.x > 0)
+        {
+            anim.SetBool("IsRight", true);
+            Debug.Log("Moving Right");
+        } else if (rb.velocity.x < 0)
+        {
+            anim.SetBool("IsLeft", true);
+            Debug.Log("Moving Left");
+        }
+        else
+        {
+            anim.SetBool("IsForward", false);
+            anim.SetBool("IsBackward", false);
+            anim.SetBool("IsRight", false);
+            anim.SetBool("IsLeft", false);
+            Debug.Log("Not Moving");
+        }
         
-        
+        // TODO: fix slow at start fast at end
         
         
     }
 
     private void FixedUpdate()
     {
+        
+        // Add diagonal movement
         if(Input.GetAxis("Vertical") > 0)
         {
             rb.AddForce(transform.forward * speed, ForceMode.Acceleration);
@@ -39,7 +67,7 @@ public class CrabMovement : MonoBehaviour
             rb.AddForce(-transform.forward * speed, ForceMode.Acceleration);
         }
 
-        // TODO: Fix over rotation
+        
         if (Input.GetAxis("Horizontal") > 0)
         {
             rb.AddForce(transform.right * speed, ForceMode.Acceleration);
