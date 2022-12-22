a3ed822cc01de8f807a99a3aab62f94444faa250
gremoryyx <arispelgremory@gmail.com>
2022-12-22 22:12:21 +0800

    Added Follow Target script for enemy

---
+++ Assets/Scripts/Enemy Scripts/FollowTarget.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class FollowTarget : MonoBehaviour
+{
+    public Transform target;
+    Vector3 destination;
+    NavMeshAgent agent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        destination = agent.destination;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update destination if the target moves one unit
+        if (Vector3.Distance(destination, target.position) > 1.0f)
+        {
+            destination = target.position;
+            agent.destination = destination;
+        }
+    }
+}
